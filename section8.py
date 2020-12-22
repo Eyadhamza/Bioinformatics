54c396ac6f183dc5572b520c90e83a5caa3f343a
EyadHamza <EyadHamza0@outlook.com>
2020-12-22 23:57:26 +0200

    some refactor

--- section8.py
+++ section8.py
@@ -9,51 +9,51 @@ d = 256
 # txt  -> text
 # q    -> A prime number
 
-def search(pat, txt, q):
-    M = len(pat)
-    N = len(txt)
+def search(pattern, text, primeNumber):
+    lengthOfPattern = len(pattern)
+    lengthOfText = len(text)
     i = 0
     j = 0
-    p = 0  # hash value for pattern
-    t = 0  # hash value for txt
+    patternHash = 0  # hash value for pattern
+    textHash = 0  # hash value for txt
     h = 1
 
     # The value of h would be "pow(d, M-1)%q"
-    for i in range(M - 1):
-        h = (h * d) % q
+    for i in range(lengthOfPattern - 1):
+        h = (h * d) % primeNumber
 
         # Calculate the hash value of pattern and first window
     # of text
-    for i in range(M):
-        p = (d * p + ord(pat[i])) % q
-        t = (d * t + ord(txt[i])) % q
+    for i in range(lengthOfPattern):
+        patternHash = (d * patternHash + ord(pattern[i])) % primeNumber
+        textHash = (d * textHash + ord(text[i])) % primeNumber
 
         # Slide the pattern over text one by one
-    for i in range(N - M + 1):
+    for i in range(lengthOfText - lengthOfPattern + 1):
         # Check the hash values of current window of text and
         # pattern if the hash values match then only check
         # for characters on by one
-        if p == t:
+        if patternHash == textHash:
             # Check for characters one by one
-            for j in range(M):
-                if txt[i + j] != pat[j]:
+            for j in range(lengthOfPattern):
+                if text[i + j] != pattern[j]:
                     break
                 else:
                     j += 1
 
-            # if p == t and pat[0...M-1] = txt[i, i+1, ...i+M-1]
-            if j == M:
+            # if patternHash == textHash and pat[0...M-1] = txt[i, i+1, ...i+M-1]
+            if j == lengthOfPattern:
                 print("Pattern found at index " + str(i))
 
                 # Calculate hash value for next window of text: Remove
         # leading digit, add trailing digit
-        if i < N - M:
-            t = (d * (t - ord(txt[i]) * h) + ord(txt[i + M])) % q
+        if i < lengthOfText - lengthOfPattern:
+            textHash = (d * (textHash - ord(text[i]) * h) + ord(text[i + lengthOfPattern])) % primeNumber
 
-            # We might get negative values of t, converting it to
+            # We might get negative values of textHash, converting it to
             # positive
-            if t < 0:
-                t = t + q
+            if textHash < 0:
+                textHash = textHash + primeNumber
 
             # Driver Code
 
