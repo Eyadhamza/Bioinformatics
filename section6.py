ef8159a06273535e33e0ceadb29c0f91d503142a
EyadHamza <EyadHamza0@outlook.com>
2020-12-22 19:58:57 +0200

    section 6

--- section6.py
+++ section6.py
@@ -29,19 +29,19 @@ def badCharHeuristic(patt, size):
     return badChar
 
 
-def search(txt, pat):
+def search(txt, pattern):
     '''
     A pattern searching function that uses Bad Character
     Heuristic of Boyer Moore Algorithm
     '''
-    n = len(pat)  # length of the pattern
-    m = len(txt)  # length of the text
+    lengthOfPattern = len(pattern)  # length of the pattern
+    lengthOfText = len(txt)  # length of the text
 
-    badChar = badCharHeuristic(pat, n)  # we construct the bad character table
+    badChar = badCharHeuristic(pattern, lengthOfPattern)  # we construct the bad character table
 
-    s = 0
-    while s <= m - n:  # we will loop until the latest alignment option
-        j = n - 1  # this line means that we are getting the value of the
+    indexOfText = 0
+    while indexOfText <= lengthOfText - lengthOfPattern:  # we will loop until the latest alignment option
+        indexOfPattern = lengthOfPattern - 1  # this line means that we are getting the value of the
         # latest element in the pattern, why ?
         # because in bad character rule we want to fine the bad character
         # that doesn't match with the character in the text
@@ -49,29 +49,29 @@ def search(txt, pat):
         # so we will start from the end then we will see if the char is good
         # we will see the next character
         # until we catch the bad character that caused the mismatch
-        while j >= 0 and pat[j] == txt[s + j]:  # this checks if its good (it matches)
-            j -= 1
+        while indexOfPattern >= 0 and pattern[indexOfPattern] == txt[indexOfText + indexOfPattern]:  # this checks if its good (it matches)
+            indexOfPattern -= 1
 
-        if j < 0:  # if you reached here it means that every character passed
+        if indexOfPattern < 0:  # if you reached here it means that every character passed
             # the previous loop right ?
             # if not? you are not lucky go to the else please
-            print("Pattern occur at index = {}".format(s))
+            print("Pattern occur at index = {}".format(indexOfText))
 
             # congrats !!!
             # now we found an occurrence find the next one now ..
             # using the bad character table constructed with the badCharHeuristic
             # method we can calculate how many shifts we will make
-            s += (n - badChar[ord(txt[s + n])] if s + n < m else 1)
-            print(s)
+            indexOfText += (lengthOfPattern - badChar[ord(txt[indexOfText + lengthOfPattern])] if indexOfText + lengthOfPattern < lengthOfText else 1)
+            print(indexOfText)
         else:  # you know why you are here ?
             # you are a bad character :((
             # we need to shift you so we can start over aren't we ?
-            # notice that j is the index of the pattern
-            # s is the index of the text
+            # notice that indexOfPattern is the index of the pattern
+            # indexOfText is the index of the text
             # using the bad character table constructed with the badCharHeuristic
             # method we can calculate how many shifts we will make
-            s += max(1, j - badChar[ord(txt[s + j])])
-            print(s)
+            indexOfText += max(1, indexOfPattern - badChar[ord(txt[indexOfText + indexOfPattern])])
+            print(indexOfText)
 
 
 # Driver program to test above function
