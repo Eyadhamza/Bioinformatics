f04d44853bec96b3513e3f7e3b1946f7cf7ae10f
EyadHamza <EyadHamza0@outlook.com>
2020-12-22 19:58:53 +0200

    section 5

--- section5.py
+++ section5.py
@@ -1,16 +1,17 @@
 # the function takes a pattern and a text
 # p is the pattern we are looking for in a sample text
 
-def naive(p, t):
+def naive(pattern, text):
     occurrences = []  # keep track of the indices of each match we had
     # now we will loop for each possible alignment
     # loop to the last position we can compare t and p
-    for i in range(len(t) - len(p) + 1):
+
+    for i in range(len(text) - len(pattern) + 1):
         # we will initialize a variable with true value and we make it false
         # if we encountered mismatch
         match = True
-        for j in range(len(p)):
-            if not t[i + j] == p[j]:
+        for j in range(len(pattern)):
+            if not text[i + j] == pattern[j]:
                 match = False
                 break
         if match:
@@ -19,7 +20,6 @@ def naive(p, t):
 
 
 t = 'AGCTTAGTAGC'
-
 p = 'AG'
 
-print(naive(p,t))
+print(naive(p, t))
